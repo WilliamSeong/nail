e1cdcee0afd2cd9f28dc09047e51e6b412a7d9ef
WilliamSeong <seongwilliam@gmail.com>
2024-12-26 22:56:29 -0500

    Added more images and instagram link

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -2,10 +2,27 @@
 </script>
 
 <template>
-  <div>
-    copyright whatever
+  <div id="footer-bar">
+    <p id="copyright">copyright</p>
+    <a href="https://www.instagram.com/amy_nailbyyoung/" target="_blank">
+      <img id="instagram-logo" src="../assets/ig-logo.png" alt="instagram">
+    </a>
   </div>
 </template>
 
 <style>
+.footer-bar {
+  width: 100%;
+  background-color: #333;
+  color: white;
+}
+
+#copyright {
+  display: inline-block;
+}
+
+#instagram-logo {
+  width: 25px;
+  display: inline-block;
+}
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,8 +2,27 @@
 </script>
 
 <template>
-  <img src="../assets/nail-img-1.jpg">
+  <div class="wall">
+    <img class="header-img" src="../assets/nail-img-1.jpg">
+    <img class="header-img" src="../assets/nail-img-2.jpg">
+    <img class="header-img" src="../assets/nail-img-3.jpg">
+  </div>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.wall {
+  height: 20%;
+  width: 100%;
+  display: inline-block;
+}
+
+.header-img {
+  width: 33.33%;
+  display: inline-block;
+}
 </style>
