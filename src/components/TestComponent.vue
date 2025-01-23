cbf4220867af817aa06115408f448ed1d92e0621
WilliamSeong <seongwilliam@gmail.com>
2025-01-22 21:42:06 -0500

    Sliding indicators done in TestComponent, integrate into Carousel

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,120 +1,109 @@
 <script setup lang="ts">
-import { ref, TransitionGroup } from 'vue';
-
+  import { ref } from 'vue';
   const imgs = [
-    "https://picsum.photos/id/237/200/300",
-    "https://picsum.photos/id/238/200/300",
-    "https://picsum.photos/id/239/200/300",
-    "https://picsum.photos/id/240/200/300",
-    "https://picsum.photos/id/241/200/300",
-    "https://picsum.photos/id/242/200/300",
-    "https://picsum.photos/id/243/200/300",
-    "https://picsum.photos/id/244/200/300",
+    "https://picsum.photos/id/237/500/500",
+    "https://picsum.photos/id/238/500/500",
+    "https://picsum.photos/id/239/500/500",
+    "https://picsum.photos/id/240/500/500",
+    "https://picsum.photos/id/241/500/500",
+    "https://picsum.photos/id/242/500/500",
+    "https://picsum.photos/id/243/500/500",
+    "https://picsum.photos/id/244/500/500",
     ]
 
     const currentSlide = ref(0);
-    const transitionTime = ref(true);
-    const indicatorCount = 5
+    const slideDirection = ref('slide-left')
 
-    function setTransitionTime() {
-      transitionTime.value = false;
-      setTimeout(() => {
-        transitionTime.value = true;
-      }, 10);
-    }
-
-    function prev() {
-      currentSlide.value--;
-      setTransitionTime();
-    }
-
-    function next() {
-      currentSlide.value++;
-      setTransitionTime();
+    function handleSlide(index : number) {
+      slideDirection.value = index > 0 ? 'slide-left' : 'slide-right'
+      currentSlide.value = (currentSlide.value + (index) + imgs.length) % imgs.length;
     }
 
 </script>
 
 <template>
-
-  <div class="test-component">
-
-      <button @click="prev">Prev</button>
-      <p>{{currentSlide}}</p>
-      <button @click="next">Next</button>
-      <br/>
-      <TransitionGroup name="slide-in">
-        <button
-          v-for="(_, index) in indicatorCount"
-          :key="index"
-          class="indicators"
-          :class="{'active':  (index < ~~(indicatorCount / 2) && (~~(indicatorCount / 2)) - index > currentSlide) ||
-                              (index > ~~(indicatorCount / 2) && index - (~~(indicatorCount / 2)) > imgs.length - 1 - currentSlide)}"
-          >
-          <!--  -->
-          <!--
-                The rule is if there are no slides before the current slide then 0 button does not show
-                If there are slides after the current slide then the 2 button does not shows
-                If there are slides before and after the current slide then all buttons show
-
-                If the differnce between the current button id and the middle button id
-                is less than the current slide and the difference is greater than the current slide
-                -->
-        </button>
-      </TransitionGroup>
-
-      <br/>
-      <img
-        v-for="(item, index) in imgs"
-        :src="item"
-        :key="index"
-        v-show="currentSlide === index"
-        alt="nail"
-        class="carousel-image">
-
-        <br/>
-
-
-        <button @click="slide"> Slide </button><br/>
-        <button
-          v-for="(_, index) in indicatorCount"
-          :key="index"
-          class="indicators"
-          >
+  <div class="indicators-container">
+    <TransitionGroup :name="slideDirection">
+      <button v-for="(n, index) in 3"
+              :key="currentSlide + n"
+              class="indicators"
+              @click="handleSlide(index - ~~(3/2))">
       </button>
+    </TransitionGroup>
   </div>
 
+  <div class="image-container">
+    <img
+      v-for="(img, index) in imgs"
+      :key="index"
+      v-show="index === currentSlide"
+      :src="imgs[index]">
+  </div>
 </template>
 
-<style scoped>
-  .test-component {
-      text-align: center;
-      margin-top: 50px;
-      background: gray;
-      height: 100vh;
-  }
-  .indicators {
-      width: 12px;
-      height: 12px;
-      border: none;
-      background: #fff;
-      margin: 0.2em;
-      opacity: .5;
-      border-radius: 50%;
-      cursor: pointer;
-  }
-  .indicators:hover {
-      opacity: 1;
-    }
-  .active {
-    opacity: 0;
-  }
+<style>
+.indicators-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    gap: 0.4em;
+    background: gray;
+}
 
-  .slide-in-enter-active{
+.indicators {
+    width: 12px;
+    height: 12px;
+    border: none;
+    background: #fff;
+    opacity: .5;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.indicators:hover {
+    opacity: 1;
+}
+
+.slide-left-move,
+.slide-right-move {
+    transition: transform 1s;
+}
+
+.slide-left-enter-active {
     transition: all 1s;
-  }
+}
 
-  .slide-in-enter-from {
-    transform: translateX(100%);
-  }
+.slide-left-leave-active {
+    transition: all 1s;
+    position: absolute;
+}
+
+.slide-left-enter-from {
+    transform: translateX(30px);
+    opacity: 0;
+}
+
+.slide-left-leave-to {
+    transform: translateX(-30px);
+    opacity: 0;
+}
+
+.slide-right-enter-active {
+    transition: all 1s;
+}
+
+.slide-right-leave-active {
+    transition: all 1s;
+    position: absolute;
+}
+
+.slide-right-enter-from {
+    transform: translateX(-30px);
+    opacity: 0;
+}
+
+.slide-right-leave-to {
+    transform: translateX(30px);
+    opacity: 0;
+}
 </style>
